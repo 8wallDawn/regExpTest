649d0bff9aca18589e625816bd3fdddfb3fa7e43
8wallDawn <dankthedust@gmail.com>
2021-10-17 00:01:53 +0900

    정규표현식 추가 예제 및 README 추가

--- README.md
+++ README.md
@@ -38,21 +38,27 @@
 
    ```jsx
    // new RegExp(표현식)
-   const regExp1 = new RegExp("^abc");
+   var regExp1 = new RegExp("^abc");
 
    // new RegExp(표현식, 플래그)
-   const regExp2 = new RegExp("^abc", "gi");
+   var regExp2 = new RegExp("^abc", "gi");
    ```
 
 1. 리터럴 방식
 
    ```jsx
    // /표현식/
-   const regExp1 = /^abc/;
+   var regExp1 = /^abc/;
 
    // /표현식/플래그
-   const regExp2 = /^abc/gi;
+   var regExp2 = /^abc/gi;
    ```
+
+### 생성자 함수와 리터럴 방식을 통한 정규표현식의 생성의 차이
+
+**생성자 함수**를 통한 방식은 **정규식 실행 시점에 컴파일**된다. 즉, 정규식의 패턴이 변경될 수 있는 경우나 사용자 입력과 같이 다른 출처로부터 패턴을 가져와야 하는 경우에 사용하는 것이 좋다.
+
+**정규식 리터럴**은 **스크립트가 불러와질 때 컴파일**된다. 정규식이 상수일 경우 다음과 같이 사용하는 것이 성능을 향상 시킬 수 있다.
 
 ## 정규표현식의 플래그
 
@@ -65,8 +71,27 @@
 - `m` (multi-line) : 다중행 검색
   `g`와 비슷하게 해석될 수 있겠으나, 다중행인 문자열의 전체를 검색하는 것이 아닌 행을 기준으로 검색한다라고 생각할 수 있다.
   즉, 행으로 나누어진 문자열마다 각각의 검색을 시도한다라고 해석하면 된다.
+  단, 다중행의 문자열을 tab 키를 이용하여 구분하였을 때, tab으로 인한 공백또한 문자열의 일부로 인식함을 주의 하여야 한다.
+
+  ```jsx
+  let person = `
+      Lee
+      dankthedust@gmail.com
+      https://github.com/8wallDawn
+  `;
+  console.log(values.match(/^l/gm)); // null
+
+  person = `
+  Lee
+  dankthedust@gmail.com
+  https://github.com/8wallDawn
+  `;
+  console.log(values.match(/^l/gm)); // ['L']
+  ```
+
 - `s` : `.` 이 개행 문자 `\n` 도 포함하도록 'dotAll' 모드를 활성화한다.
   다소 이해가 안될 수 있겠으며, 코드를 보면서 이해해 본다.
+
   ```jsx
   var str1 = "bar\nexample foo example";
   var regex1 = new RegExp("bar.example", "s");
@@ -81,9 +106,121 @@
   console.log(str2.replace(regex2, "")); // Output: bar
   //         example foo example
   ```
+
   코드에서 보듯 `str1` 에 존재하는 개행문자 `\n` 의 값을 인식하는 지 확인 하는 역할이 바로 `RegExp.prototype.dotAll;` 이며, 이를 활성화하는 것이 `s`이다.
+
 - `u` (unicode) : 유니코드
 - `y` (sticky) : "sticky" 검색을 통해 문자열의 특정 위치부터 검색을 수행한다.
+
+## 패턴(표현)
+
+---
+
+### [패턴(표현식) 모음](https://developer.mozilla.org/ko/docs/Web/JavaScript/Guide/Regular_Expressions#.ed.8c.a8.ed.84.b4.ed.99.94.eb.90.9c_.eb.b6.80.eb.b6.84_.eb.ac.b8.ec.9e.90.ec.97.b4_.ec.9d.bc.ec.b9.98_.ec.82.ac.ec.9a.a9.ed.95.98.ea.b8.b0)
+
+### 자주 사용하는 패턴
+
+- `^`와 `$` : 캐럿 기호 ^는 텍스트의 시작, 달러 기호 $는 텍스트의 끝
+  ```jsx
+  let str = `
+  Kim A
+  Park B
+  Lee C
+  `;
+  console.log(str.match(/^kim/gim)); // ['Kim']
+  console.log(str.match(/b$/gim)); // ['B']
+  ```
+- `.` : 임의의 한 문자와 일치
+  ```jsx
+  let str = `
+  abcd
+  a12d
+  a!@d
+  `;
+  console.log(str.match(/a..d/gm)); // ['abcd', 'a12d', 'a!@d']
+  ```
+- `a|b` : a 또는 b
+  ```jsx
+  let str = `
+  Kim A
+  Park B
+  Lee C
+  `;
+  console.log(str.match(/kim|park/gi)); // ['Kim', 'Park']
+  console.log(str.match(/kim|choi|lee/i)); // ['Kim']
+  ```
+- `ab?` : 문자 b가 있거나 없는 경우
+  ```jsx
+  let str = `
+  http://github.com/8wallDawn
+  https://github.com/8wallDawn
+  `;
+  console.log(str.match(/https?/g));
+  ["http", "https"];
+  ```
+- `{numX*[, numY]*}` : 한 문자의 numX 만큼의 연속 일치
+  ```jsx
+  let str = `
+  010-1134-56667
+  dankthedust@gmail.com
+  https://github.com/8wallDawn
+  `;
+  console.log(str.match(/1{2}/g)); // ['11']
+  console.log(str.match(/\d{2,}/g)); // ['010', '1134', '56667']
+  console.log(str.match(/\b\w{3,5}\b/g)); // ['010', '1134', '56667', 'gmail', 'com', 'https', 'com']
+  ```
+- `[]` : 특정 구간의 문자 찾기
+  ```jsx
+  str = `aAbBcCdDeE01234가나다라!@#$%`;
+  ```
+  - `[abc]` : a 또는 b또는 c
+    ```jsx
+    console.log(str.match(/[d3다!]/g)); // ['d', '3', '다', '!']
+    ```
+  - `[a-z]` : a부터 z 사이의 문자 구간(소문자)
+    ```jsx
+    console.log(str.match(/[c-f]/g)); // ['c', 'd', 'e']
+    ```
+  - `[A-Z]` : A부터 Z 사이의 문자 구간(대문자)
+    ```jsx
+    console.log(str.match(/[D-E]/g)); // ['D', 'E']
+    ```
+  - `[0-9]` : 0부터 9 까지의 문자 구간(숫자)
+    ```jsx
+    console.log(str.match(/[2-5]/g)); // ['2', '3', '4']
+    ```
+  - `[가-힣]` : 가부터 힣 까지의 문자 구간(한글)
+    ```jsx
+    console.log(str.match(/[다-라]/g)); // ['다', '라']
+    ```
+- `\w` : 63개의 문자(대소문자 52개 + 숫자 10개 + \_ ) 에 일치
+- `\b` : 63개 문자(`\w`) 를 제외하는 나머지 문자의 경계(Boundary)
+- `\d` : 숫자(Digit)에 일치
+- `\s` : 공백(Space, Tab, enter 등) 에 일치
+
+```jsx
+let str = `fire fox apple apple_fox fox_apple`;
+// 소문자 f로 시작하는 단어 찾기
+console.log(str.match(/\bf\w{1,}\b/g)); // ['fire', 'fox', 'fox_apple']
+
+// \s 를 활용한 공백 지우기
+let str = `   hel lo New W  or
+l
+d!!
+`;
+console.log(str.replace(/\s/g, "")); //helloNewWorld!!
+```
+
+- `(?=)` 와 `(?<=)` : 앞쪽 일치와 뒤쪽 일치
+  ```jsx
+  str = `
+  dankthedust@gmail.com
+  `;
+  console.log(
+    str.match(/.{1,}(?=@)/g), // ['dankthedust']
+    str.match(/(?<=@).{1,}/g) // ['gmail.com']
+  );
+  ```
 
 ## 정규식을 활용하는 다양한 메소드
 
--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-const values = `
+let values = `
     WELCOME
     Lee
+    lee
     dankthedust@gmail.com
     seoul
     https://github.com/8wallDawn
@@ -10,6 +11,8 @@ const values = `
 let reg = new RegExp('com','');
 console.log(values.match(reg)) // Array(1) 단일 항목 반환
 
+/* ---------- */
+// 플래그를 활용한 검색
 reg = new RegExp('com','g'); // 플래그 'g'
 console.log(values.match(reg)) // Array(2) 전체 항목 반환
 // 플래그 'g'(global)는 모든 항목을 반환한다.
@@ -18,17 +21,108 @@ reg = new RegExp('com','gi'); // 플래그 'g'와 'i'
 console.log(values.match(reg)) // Array(3) 전체 항목 반환
 // 플래그 'i'(ignoreCase)는 대소문자를 구분하지 않는다.
 
+/* ---------- */
+values = `
+    Lee
+    leee
+    dankthedust@gmail.com
+    https://github.com/8wallDawn
+    http://github.com/8wallDawn
+`
+// 자주 사용하는 패턴(표현식)을 활용한 검색
+// /abc$/ : 텍스트의 끝이 abc
+console.log('find letters end m', values.match(/m$/gm));
 
+// /^abc/ : 텍스트의 시작이 abc
+console.log('find letters begin l', values.match(/^l/gim)); // null
+/*
+null 인 이유는 `` 사이에 줄바꿈 처리한 내용에 tab을 이용하여 생긴 공백또한 문자열의 일부로 인식하기 때문에 모든 줄의 tab공백을 지우면 정상적으로 검색이 된다.
+*/
+values = `
+Lee
+leee
+dankthedust@gmail.com
+https://github.com/8wallDawn
+http://github.com/8wallDawn
+`
+console.log('find letters begin l', values.match(/^l/gim)); // ['L', 'l']
 
+// /./ : 임의의 한 문자와 일치
+console.log('two letter between gi and ub', values.match(/gi..ub/g)); // ['github', 'github']
+
+// /a|b/ : a또는 b와 일치
+console.log('lee or Lee', values.match(/lee|Lee/g)); // ['Lee','lee'] ,플래그 g가 없는 경우 Lee가 먼저 찾아져 ['Lee']만을 리턴
+
+// /ab?/ : b가 존재하거나 존재하지 않은 경우와 일치
+console.log('s is exist or not', values.match(/https?/g)); // ['https', 'http']
+
+// /a{num}/ : num의 값 만큼 한 문자 a에 대한 연속 일치
+// /a{numX,}/ : numX 이상 한 문자 a에 대한 연속 일치
+// /a{numX,numY}/ : numX 이상 numY 이하 한 문자 a에 대한 연속 일치
+console.log(
+    values.match(/e{2}/), // ['ee']
+    values.match(/e{3,}/), // ['ee']
+    values.match(/e{2,3}/g), // ['ee', 'eee']
+    // 2~5개 사이의 \b(특수문자, _제외) 사이의 \w(숫자나 문자, _)를 검색
+    values.match(/\b\w{2,5}\b/g) // ['Lee', 'leee', 'gmail', 'com', 'https', 'com', 'http', 'com']
+)
+
+// [] 특정구간의 문자 찾기
+let str = `aAbBcCdDeE01234가나다라!@#$%`;
+// [abc] : a 또는 b또는 c
+console.log(str.match(/[d3다!]/g)); // ['d', '3', '다', '!']
+//[a-z] : a부터 z 사이의 문자 구간(소문자)
+console.log(str.match(/[c-f]/g)); // ['c', 'd', 'e']
+//[A-Z] : A부터 Z 사이의 문자 구간(대문자)
+console.log(str.match(/[D-E]/g)); // ['D', 'E']
+//[0-9] : 0부터 9 까지의 문자 구간(숫자)
+console.log(str.match(/[2-5]/g)); // ['2', '3', '4']
+//[가-힣] : 가부터 힣 까지의 문자 구간(한글)
+console.log(str.match(/[다-라]/g)); // ['다', '라']
+
+// 특정 문자 타입에 대한 일치
+// \w : 63개의 문자(대소문자 52개 + 숫자 10개 + _ ) 에 일치
+// \b : 63개 문자(`\w`) 를 제외하는 나머지 문자의 경계(Boundary)
+// \d : 숫자(Digit)에 일치
+// \s : 공백(Space, Tab, enter 등) 에 일치
+str = `fire fox apple apple_fox fox_apple`
+// 소문자 f로 시작하는 단어 찾기
+console.log(str.match(/\bf\w{1,}\b/g)) // ['fire', 'fox', 'fox_apple']
+
+// \s 를 활용한 공백 지우기
+str = `   hel lo New W  or
+l
+d!!
+`
+console.log(str.replace(/\s/g, '')); //helloNewWorld!!
+
+// (?=) 과 (?<=) : 앞쪽 일치와 뒤쪽 일치
+str = `
+dankthedust@gmail.com
+`
+console.log(
+	str.match(/.{1,}(?=@)/g), // ['dankthedust']
+	str.match(/(?<=@).{1,}/g) // ['gmail.com']
+)
+
+/* ---------- */
 // 정규표현식을 활용하는 다양한 메소드 활용 예제.
-const str = `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`;
+const letters = `Lorem Ipsum is simply dummy text of the printing and typesetting indulettersy. Lorem Ipsum has been the indulettersy's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`;
 const replaceText = `LOREM`;
 
-reg = new RegExp('Lorem', 'gi');
-console.log('1',reg.exec(str));
-console.log('2',reg.test(str));
-console.log('3',str.match(reg));
-console.log('4',str.search(reg));
-console.log('5',str.replace(reg, replaceText));
-console.log('6',str.split(reg));
-console.log('7',reg.toString());
+reg = new RegExp('Lorem', 'gi'); 
+console.log('1',reg.exec(letters)); // Array(1)
+console.log('2',reg.test(letters)); // true
+console.log('3',letters.match(reg)); // Array(4)
+console.log('4',letters.search(reg)); // 0
+console.log('5',letters.replace(reg, replaceText)); // LOREM Ipsum is simply dummy text of the printing ...
+console.log('6',letters.split(reg)); // Array(5)
+/*
+0: ""
+1: " Ipsum is simply dummy text of the printing and typesetting indulettersy. "
+2: " Ipsum has been the indulettersy's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing "
+3: " Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of "
+4: " Ipsum."
+length: 5
+*/
+console.log('7',reg.toString()); // /Lorem/gi
